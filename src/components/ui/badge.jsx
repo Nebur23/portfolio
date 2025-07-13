eddc8278aaaa59d39f600cc64e91da4ff3fd5a14
Nebur23 <yvesnyemb7@gmail.com>
2025-07-13 14:53:27 +0100

    Updates

--- src/components/ui/badge.jsx
+++
@@ -1,9 +0,0 @@
-const Badge = ({ text }) => {
-  return (
-    <span className='rounded-full border border-accent px-2.5 py-0.5 text-sm whitespace-nowrap text-accent font-semibold'>
-      {text}
-    </span>
-  );
-};
-
-export default Badge;
